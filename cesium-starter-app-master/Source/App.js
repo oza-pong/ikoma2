13e8166b84045e192f12ac59a78a1eb749fcff8b
oza-pong <risei.heian.kashiba@gmail.com>
2017-01-14 16:38:40 +0900

    Update App.js

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,3 +1,3 @@
 var cesiumWidget = new Cesium.Viewer('cesiumContainer');
 
-viewer.dataSources.add(Cesium.KmlDataSource.load('http://oza-pong.github.io/ikoma2/ikoma4.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('http://oza-pong.github.io/ikoma2/cesium-starter-app-master/Source/ikoma4.kml'));
