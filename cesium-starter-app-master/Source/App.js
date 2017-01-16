d69c4aa3bc8c2d1ec1e9f08df2350336e98978b6
oza-pong <risei.heian.kashiba@gmail.com>
2017-01-16 16:02:57 +0900

    r

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -4,4 +4,4 @@ var viewer = new Cesium.Viewer('cesiumContainer',{
 });
 viewer.camera.setView({
   destination: Cesium.Cartesian3.fromDegrees(135.695915, 34.692792, 45000.0)});
-viewer.dataSources.add(Cesium.KmlDataSource.load('kml/ikoma.kml'));
+viewer.dataSources.add(Cesium.KmlDataSource.load('kml/ikoma4.kml'));
